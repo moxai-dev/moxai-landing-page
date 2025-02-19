2c9c3725b5b22c1da97a0d412709f78c9e6958b8
Daniel Morshinin <geelix@mail.ru>
2025-02-19 20:30:31 +0300

    Add PrimeVue. Add --host param in npm run dev

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import './assets/main.css'
+import 'primeicons/primeicons.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import { ru as primeVueLocaleRu } from 'primelocale/ru.json'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: { preset: Aura },
+  locale: { ...primeVueLocaleRu },
+})
+
+app.mount('#app')
