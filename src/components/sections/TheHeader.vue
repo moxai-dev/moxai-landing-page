7adf89311f9c740b698019e9dd0bc989b837fe08
Daniel Morshinin <geelix@mail.ru>
2025-03-13 10:09:07 +0300

    Improve TheHeader

--- src/components/sections/TheHeader.vue
+++ src/components/sections/TheHeader.vue
@@ -3,20 +3,20 @@ import { onMounted, ref } from 'vue'
 import Drawer from 'primevue/drawer'
 import MoxaiLogoIcon from '@/components/icons/MoxaiLogoIcon.vue'
 
-const visible = ref(false)
+const isDrawerVisible = ref(false)
+
+const openDrawer = () => {
+  isDrawerVisible.value = true
+}
+
+const closeDrawer = () => {
+  isDrawerVisible.value = false
+}
 
 const isPreferableDarkMode = localStorage.getItem('theme-mode') == 'dark'
 const isPreferableLightMode = localStorage.getItem('theme-mode') == 'light'
 const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)')
 const isDarkMode = ref(!isPreferableLightMode && (mediaQueryList.matches || isPreferableDarkMode))
-
-const openDrawer = () => {
-  visible.value = true
-}
-
-const closeDrawer = () => {
-  visible.value = false
-}
 
 const toggleDarkMode = () => {
   if (isPreferableDarkMode || isPreferableLightMode) {
@@ -57,7 +57,7 @@ onMounted(() => {
 
     <i class="pi pi-bars !text-2xl text-slate-800 dark:text-slate-100" @click="openDrawer"></i>
 
-    <Drawer v-model:visible="visible" position="right" header="Drawer">
+    <Drawer v-model:visible="isDrawerVisible" position="right" header="Меню">
       <div class="flex flex-col gap-y-5">
         <a href="#problem" @click="closeDrawer">О проблеме</a>
         <a href="#solution" @click="closeDrawer">Наше решение</a>
